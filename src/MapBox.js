8c52da3edcbafdab08783fe9b54bf56e10568436
Gshopping <zays.kim@gmail.com>
2020-08-24 22:02:59 +0900

    Adding Sample code for Mapbox integration

--- src/MapBox.js
+++ src/MapBox.js
@@ -1,23 +1,56 @@
-// src/CardList.js
+// src/MapBox.js
 
-import React from 'react';
+/****************************************
+ * File : MapBox.js
+ * In this file, Mapbox is implemented with react hook.
+ * 
+ * Author : Jeesub Kim
+ ****************************************/
+import React, {useEffect, useState, useRef} from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import mapboxgl from 'mapbox-gl';
+import "mapbox-gl/dist/mapbox-gl.css";
+mapboxgl.accessToken = process.env.REACT_MAPBOX_KEY;
+
+const styles = {
+    width: "100vw",
+    height: "calc(100vh - 80px)",
+    position: "absolute"
+  };
+
 const Wrapper = styled.div``;
 
-class CardList extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {};
-    }
-
-    render() {
-        return <Wrapper></Wrapper>;
-    }
+function MapBox(props) {
+    const [map, setMap] = useState(null);
+    const mapContainer = useRef(null);
+    useEffect( ()=>{
+    
+        const initMap = ({setMap, mapContainer}) =>{
+          const map = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [5,34],
+            zoom: 2
+          });
+    
+          map.on('load', () =>{
+            setMap(map);
+            map.resize();
+          });
+        };
+    
+        if(!map)
+          initMap({setMap, mapContainer});
+    
+      },[map]);
+      
+      return (
+        <div>
+        <div ref = {el => mapContainer.current = el} style={styles}/>
+      </div>
+      )
 }
 
-CardList.propTypes = {};
-
-export default CardList;
+export default MapBox;
