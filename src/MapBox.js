b5d3dc1b67253b11826a295bcee7db785c6e6fe2
Gshopping <zays.kim@gmail.com>
2020-09-03 01:42:47 +0900

    Removal of unused codes and adding features for markers, display and closeOnClick

--- src/MapBox.js
+++ src/MapBox.js
@@ -10,117 +10,18 @@ import React, {useEffect, useState, useRef} from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-// import Box from '@material-ui/core';
-
-import defaultIcon from './mapbox-icon.png'
 import mapboxgl from 'mapbox-gl';
 import "mapbox-gl/dist/mapbox-gl.css";
-
-/**********************Sample input ********************************************************************************************
- * 
- * const sampleInputs = {
-  data:{
-      mapboxKey:'pk.eyJ1IjoiamVlc3ViIiwiYSI6ImNrZTAxemlnOTBmZmYycnA3aG8wcmo1eWkifQ.wTZ5zzP0m-nh8JrhcIu94A',
-      title:'Stickyboard-mapbox example',
-      description: 'This component is one of stickers in Stickyboard',
-  },
-  map:{
-      camera:{
-      center: [-122.486052, 37.830348],
-      zoom: 10
-      },
-      
-      marker: [{
-      coordinates: [-77.032, 38.913],
-      title: 'Mapbox',
-      description: 'Washington, D.C.'
-      },{
-          coordinates: [-122.414, 37.776],
-          title: 'Mapbox',
-          description: 'San Francisco, California'
-      }],
-      
-      line:[{
-      paint:{
-          lineColor : '#888',
-          lineWidth: 8
-      },
-      coordinates: [
-          [-122.48369693756104, 37.83381888486939],
-          [-122.48348236083984, 37.83317489144141],
-          [-122.48339653015138, 37.83270036637107],
-          [-122.48356819152832, 37.832056363179625],
-          [-122.48404026031496, 37.83114119107971],
-          [-122.48404026031496, 37.83049717427869],
-          [-122.48348236083984, 37.829920943955045],
-          [-122.48356819152832, 37.82954808664175],
-          [-122.48507022857666, 37.82944639795659],
-          [-122.48610019683838, 37.82880236636284],
-          [-122.48695850372314, 37.82931081282506],
-          [-122.48700141906738, 37.83080223556934],
-          [-122.48751640319824, 37.83168351665737],
-          [-122.48803138732912, 37.832158048267786],
-          [-122.48888969421387, 37.83297152392784],
-          [-122.48987674713133, 37.83263257682617],
-          [-122.49043464660643, 37.832937629287755],
-          [-122.49125003814696, 37.832429207817725],
-          [-122.49163627624512, 37.832564787218985],
-          [-122.49223709106445, 37.83337825839438],
-          [-122.49378204345702, 37.83368330777276]
-      ],
-      title: 'line#1',
-      description: 'line#1 description here'
-      
-      },{
-          paint:{
-              lineColor : '#222',
-              lineWidth: 8
-          },
-          coordinates: [
-              [-112.48369693756104, 37.83381888486939],
-              [-112.48348236083984, 37.83317489144141],
-              [-112.48339653015138, 37.83270036637107],
-              [-112.48356819152832, 37.832056363179625],
-              [-112.48404026031496, 37.83114119107971],
-              [-112.48404026031496, 37.83049717427869],
-              [-112.48348236083984, 37.829920943955045],
-              [-112.48356819152832, 37.82954808664175],
-              [-112.48507022857666, 37.82944639795659],
-              [-112.48610019683838, 37.82880236636284],
-              [-112.48695850372314, 37.82931081282506],
-              [-112.48700141906738, 37.83080223556934],
-              [-112.48751640319824, 37.83168351665737],
-              [-112.48803138732912, 37.832158048267786],
-              [-112.48888969421387, 37.83297152392784],
-              [-112.48987674713133, 37.83263257682617],
-              [-112.49043464660643, 37.832937629287755],
-              [-112.49125003814696, 37.832429207817725],
-              [-112.49163627624512, 37.832564787218985],
-              [-112.49223709106445, 37.83337825839438],
-              [-112.49378204345702, 37.83368330777276]
-          ],
-          title: 'line#1',
-          description: 'line#1 description here'
-      }],
-  }
-}
-
- ******************************************************************************************************************************************/
-const styles = {
-    width: "100vw",
-    height: "calc(100vh - 80px)",
-    position: "absolute"
-  };
 
 const Wrapper = styled.div`
     height: 100%;
     padding: 8px;
-    overflow: scroll;
+    overflow: hidden;
 `;
 
 const getMarkerStyle= (icon) => {
   return {
-    backgroundImage: `url(${icon})`,
+    backgroundImage: icon,
     backgroundSize: 'cover',
     width: '50px',
     height: '50px',
@@ -135,7 +36,11 @@ function MapBox(props) {
     
     const [map, setMap] = useState(null);
     const mapContainer = useRef(null);
-    
+    const styles = {
+      width: props.map.canvas.size.width,
+      height: props.map.canvas.size.height,
+    }
+
     const initMap = ({setMap, mapContainer}) =>{
       const mMap = new mapboxgl.Map({
         container: mapContainer.current,
@@ -154,23 +59,12 @@ function MapBox(props) {
     const drawMarkers = ()=>{
 
       if(map){
-        console.log(props.map.marker);
         props.map.marker.forEach( (mark)=>{
   
-          console.log(mark);
-          // let markerElement = document.createElement('div');
-
-          let reactMarker = React.createElement('div',getMarkerStyle(mark.img?mark.img:defaultIcon),'styled');
-          // markerElement.style = getMarkerStyle(mark.img?mark.img:defaultIcon);
-          // markerElement.style.cssText=`background-image: url(${defaultIcon});
-          // background-size: cover;
-          // width: 50px;
-          // height: 50px;
-          // border-radius: 50%;
-          // cursor: pointer;`;
-          
-
-          new mapboxgl.Marker(reactMarker).setLngLat(mark.coordinates).setPopup(new mapboxgl.Popup({offset:25}).setHTML('<h3>'+mark.title + '</h3><p>'+mark.description +'</p>')).addTo(map);          
+          let reactMarker = React.createElement('div',getMarkerStyle(mark.img),'styled');
+          let marker = new mapboxgl.Marker(reactMarker).setLngLat(mark.coordinates).setPopup(new mapboxgl.Popup({offset:25,closeOnClick: mark.closeOnClick }).setHTML('<h3>'+mark.title + '</h3><p>'+mark.description +'</p>')).addTo(map);          
+          if(mark.display)
+            marker.getPopup().addTo(map);
         })
       }
       
@@ -215,7 +109,6 @@ function MapBox(props) {
             }
           }
 
-          console.log(id,route,layer);
           map.addSource(id,route);
           map.addLayer(layer);
         })
@@ -246,16 +139,23 @@ MapBox.propTypes ={
     description: PropTypes.string,
   }),
   map:PropTypes.shape({
-    camera:{
+    canvas:PropTypes.shape({
+      size:PropTypes.shape({
+        width:PropTypes.string,
+        height:PropTypes.string
+      })
+    }),
+    camera:PropTypes.shape({
       center: PropTypes.array,
       zoom: PropTypes.number
-    },
+    }),
     
     marker: PropTypes.arrayOf(PropTypes.shape({
       img:PropTypes.string,//defaultIcon,
       coordinates: PropTypes.array,
       title: PropTypes.string,
-      description: PropTypes.string
+      description: PropTypes.string,
+      display:PropTypes.bool
       
     })),
     
