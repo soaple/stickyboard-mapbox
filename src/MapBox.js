b20c4316de3af6c6906cb7dab7d3cebbfe61bc6c
Gshopping <zays.kim@gmail.com>
2020-09-11 22:10:00 +0900

    Adding geolocation and openweathermap

--- src/MapBox.js
+++ src/MapBox.js
@@ -11,6 +11,10 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import mapboxgl from 'mapbox-gl';
+
+
+import Axios from 'axios'
+
 import "mapbox-gl/dist/mapbox-gl.css";
 
 const Wrapper = styled.div`
@@ -48,8 +52,6 @@ const building3dLayer = {
   'paint': {
     'fill-extrusion-color': '#aaa',
     
-    // use an 'interpolate' expression to add a smooth transition effect to the
-    // buildings as the user zooms in
     'fill-extrusion-height': [
         'interpolate',
         ['linear'],
@@ -58,8 +60,8 @@ const building3dLayer = {
         0,
         15.05,
         ['get', 'height']
-      ],
-      'fill-extrusion-base': [
+    ],
+    'fill-extrusion-base': [
         'interpolate',
         ['linear'],
         ['zoom'],
@@ -67,31 +69,32 @@ const building3dLayer = {
         0,
         15.05,
         ['get', 'min_height']
-      ],
+    ],
     'fill-extrusion-opacity': 0.6
   }
 }
-  const add3dBuildingLayer = (map) => {
+const add3dBuildingLayer = (map) => {
 
-    var layers = map.getStyle().layers;
-          
-    var labelLayerId;
-    for (var i = 0; i < layers.length; i++) {
-      if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-        labelLayerId = layers[i].id;
-        break;
-      }
+  var layers = map.getStyle().layers;
+        
+  var labelLayerId;
+  for (var i = 0; i < layers.length; i++) {
+    if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+      labelLayerId = layers[i].id;
+      break;
     }
-
-    map.addLayer(
-      building3dLayer,
-      labelLayerId
-    );
   }
+
+  map.addLayer(
+    building3dLayer,
+    labelLayerId
+  );
+}
+
 function MapBox(props) {
   
     mapboxgl.accessToken = props.data.mapboxKey;
-
+    
     
     const [map, setMap] = useState(null);
     const mapContainer = useRef(null);
@@ -102,12 +105,19 @@ function MapBox(props) {
 
     const initMap = ({setMap, mapContainer}) =>{
       
+      
       let mapOption = {
         container: mapContainer.current,
         style: (props.map.canvas.style !== undefined)? mapboxStyle[props.map.canvas.style]: 'mapbox://styles/mapbox/streets-v11',
         center: props.map.camera.center,
         zoom: props.map.camera.zoom,
+        
       }
+      if(props.map.canvas.interactive !== undefined){
+        mapOption.interactive = props.map.canvas.interactive;
+      }
+      
+      
       if(props.map.camera.pitch !== undefined)
         mapOption.pitch = props.map.camera.pitch;
       if(props.map.camera.bearing !== undefined)
@@ -117,21 +127,112 @@ function MapBox(props) {
         mapOption.maxZoom = props.slideshow.maxZoom
         mapOption.minZoom = props.slideshow.minZoom 
       }
-      
+      console.log(mapOption);
       const mMap = new mapboxgl.Map(mapOption);
       
+      
+      
+      
+        
       
       if(!props.map.canvas.scrollZoom)
         mMap.scrollZoom.disable();
 
-        mMap.on('load', () => {
-          mMap.resize();
-          if(props.map.canvas.building3d){
-            add3dBuildingLayer(mMap);
+      
+
+      mMap.on('load', () => {
+        mMap.resize();
+        if(props.map.canvas.building3d){
+          add3dBuildingLayer(mMap);
+        }
+
+        if(props.map.canvas.geoLocation)
+        {
+          if(!navigator.geolocation){
+            console.log('Geolocation is not supported by this browser');
+          }else{
+            console.log('Getting current position');
+            navigator.geolocation.getCurrentPosition( position => {
+              
+              
+              const urlByGps = 'https://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&appid=' + props.data.openweathermapKey;
+              // const urlByGps = 'api.openweathermap.org/data/2.5/weather?lat=' + 37 + '&lon=' + 127 + '&appid=' + props.data.openweathermapKey;
+              // const urlByGps = 'https://api.openweathermap.org/data/2.5/weather?q=Seoul' + '&appid=' + props.data.openweathermapKey;
+              
+              const loc = {
+                center:[position.coords.longitude,position.coords.latitude],
+                zoom:14,
+                pitch:60,
+                bearing:-60,
+              }
+              mMap.flyTo(loc);
+              Axios.get(urlByGps).then( res => {
+                    const data = res.data;
+                    console.log(data);
+                    const weatherInformation = {
+
+                      name : data.name,
+                      rain : data.rain['1h'],
+                      clouds : data.clouds.all,
+                      
+                      weather : data.weather[0],
+                      feels_like : (data.main.feels_like - 273).toFixed(2),
+                      humidity : data.main.humidity,
+                      pressure : data.main.pressure,
+                      temp : (data.main.temp - 273).toFixed(2),
+                      temp_max : (data.main.temp_max - 273).toFixed(2),
+                      temp_min : (data.main.temp_min - 273).toFixed(2),
+                      wind : data.wind,
+                      
+                      loading : false,
+                      
+                    }
+                    const imgSrc = 'http://openweathermap.com/img/w/' + weatherInformation.weather.icon + '.png';
+                    
+                    mMap.once('moveend', () => {
+
+                      let markerElement = document.createElement('div');
+                      markerElement.style.cssText=`background-image: url(${imgSrc});
+                      background-size: cover;
+                      width: 100px;
+                      height: 100px;
+                      border-radius: 50%;
+                      cursor: pointer;`;
+
+                      // let reactMarker = React.createElement('div',getMarkerStyle(imgSrc),'styled');
+                      let marker = new mapboxgl.Marker(markerElement).setLngLat(loc.center).setPopup(new mapboxgl.Popup({offset:25})
+                      .setHTML(`<h2> ${weatherInformation.name} </h2>
+                       <p> 
+                      <h3>
+                       It's ${weatherInformation.weather.main}y (${weatherInformation.weather.description})<br>
+                       Feels like ${weatherInformation.feels_like} 'C <br>
+
+                      </h3>
+
+                       Rain : ${weatherInformation.rain} per hour<br>
+                       Temperature : ${weatherInformation.temp} 'C <br>
+                                     - MAX : ${weatherInformation.temp_max} 'C <br>
+                                     - MIN : ${weatherInformation.temp_min} 'C <br>
+                       Humidity : ${weatherInformation.humidity}, 
+                       Clouds : ${weatherInformation.clouds}<br>
+                       Wind : Speed - ${weatherInformation.wind.speed}, Degree : ${weatherInformation.wind.deg}<br>
+                       <br>
+
+                       
+                      
+                      </p>`)).addTo(mMap);
+                      marker.getPopup().addTo(mMap);
+                    });
+              }).catch( err => console.log(err) );
+             
+
+            })
           }
-          setMap(mMap);
-          
-        });
+        } 
+        
+        setMap(mMap);
+        
+      });
         
       
     };
@@ -285,6 +386,7 @@ function MapBox(props) {
 MapBox.propTypes ={
   data:PropTypes.shape({
     mapboxKey:PropTypes.string.isRequired,
+    openweathermapKey:PropTypes.string,
     title:PropTypes.string,
     description: PropTypes.string,
   }).isRequired,
@@ -296,7 +398,9 @@ MapBox.propTypes ={
         width:PropTypes.string,
         height:PropTypes.string
       }),
-      scrollZoom:PropTypes.bool
+      scrollZoom:PropTypes.bool,
+      geoLocation:PropTypes.bool,
+      interactive:PropTypes.bool
     }),
     camera:PropTypes.shape({
       center: PropTypes.array,
